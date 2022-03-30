480f60dea5759a1986cc5eb0565aafc337c52d67
ghryu <dodnam@naver.com>
2022-03-30 21:17:49 +0900

    기본 설정

---
+++ src/main/java/com/motion/common/controller/CommonController.java
@@ -0,0 +1,15 @@
+package com.motion.common.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CommonController {
+
+	  @GetMapping("/")
+	  public String index() {
+	    return "demo project!!";
+	  }
+}
+
